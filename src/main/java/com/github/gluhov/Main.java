dc6856b75e78af03ceb47705c754fd909a2ebe86
gluhov-d <glukhov.d@gmail.com>
2023-11-07 01:14:40 +0300

    readme added

--- src/main/java/com/github/gluhov/Main.java
+++ src/main/java/com/github/gluhov/Main.java
@@ -1,4 +1,6 @@
-package com.github.gluhov;public class Main {
+package com.github.gluhov;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
